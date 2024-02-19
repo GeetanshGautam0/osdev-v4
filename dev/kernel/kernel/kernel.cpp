c1dba33bf56d60cea23e780c52ee6851c8f1257b
GeetanshGautam0 <loveplanes@hotmail.com>
2024-02-18 19:07:08 -0800

    Made dev branch in preparation for branch protection

--- dev/kernel/kernel/kernel.cpp
+++ dev/kernel/kernel/kernel.cpp
@@ -7,6 +7,7 @@ void kernel_main
     TTY tty = TTY(VGA_COLOR::WHITE, VGA_COLOR::BLUE, (dimensions_t){80, 25});
     tty.ClearScreen();
     tty.Write("Hello, Kernel!");
+    tty.Write("Wow this actually works.");
 
     tty.SetColor(VGA_COLOR::LIGHT_BROWN, VGA_COLOR::BLACK);
 
