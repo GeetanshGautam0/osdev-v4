54f70370cf4acace9846dee62ebee61d7edfc914
GeetanshGautam0 <loveplanes@hotmail.com>
2024-02-24 23:36:36 -0800

    PRINTF-16, PUTS, PUTS_FAR, PUTC

--- src/bootloader/stage2/c_stdio.h
+++ src/bootloader/stage2/c_stdio.h
@@ -3,6 +3,9 @@
 
 void putc ( char c );
 void puts ( const char * s );
+void puts_f ( const char __far * s );
+
+void _cdecl printf16 ( const char * __restrict fmt, ... );
 
 #endif /* C_STDIO_H */
 
--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -3,6 +3,51 @@ bits 16
 section _TEST class=CODE
 
 ;
+; void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotientOut, uint32_t* remainderOut);
+;                           ^ eax               ^ ecx
+;
+global _x86_div64_32
+_x86_div64_32:
+
+    ; make new call frame
+    push bp             ; save old call frame
+    mov bp, sp          ; initialize new call frame
+
+    push bx
+
+    ; ----------------------------- x86 DIV -------------------------
+
+    ; Divide upper 32 bits of the divided
+    mov eax, [bp + 8]   ; eax = upper 32 bits of dividend (dividend >> 8) & 0xFF
+    mov ecx, [bp + 12]  ; ecx = divisor
+    xor edx, edx        ; edx = 0
+    div ecx             ; eax = divided/divisor, edx dividend (mod divisor)
+
+    ; store upper 32 bits of quotient
+    mov bx, [bp + 16]
+    mov [bx + 4], eax
+
+    ; divide lower 32 bits
+    mov eax, [bp + 4]   ; eax <- lower 32 bits of dividend
+                        ; edx <- old remainder
+    div ecx
+
+    ; store results
+    mov [bx], eax
+    mov bx, [bp + 18]
+    mov [bx], edx
+
+    ; ----------------------------- x86 DIV -------------------------
+
+    ; restore bx
+    pop bx
+
+    ; restore old call frame
+    mov sp, bp
+    pop bp
+    ret
+
+;
 ;   _x86_64_WriteCharTeletype
 ;   Using:
 ;       INT 10h with ah = 0Eh
